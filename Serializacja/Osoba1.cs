a9e4ce604b248e9d37bd9b90ba9300c5eec44415
MrSzpachla <kamilpaszkiet@gmail.com>
2022-06-25 10:02:44 +0200

    Dodaj pliki projektów.

---
+++ Serializacja/Osoba1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serializacja
+{
+    public class Osoba1
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int Wiek { get; set; }
+        public void Save(string nazwaPliku)
+        {
+            using (var stream = new FileStream(nazwaPliku, FileMode.Create))
+            {
+                var XML = new XmlSerializer(typeof(Osoba1));
+                XML.Serialize(stream, this);
+            }
+        }
+        public static Osoba1 Load(string nazwaPliku)
+        {
+            using(var stream = new FileStream(nazwaPliku, FileMode.Open))
+            {
+                var XML = new XmlSerializer(typeof(Osoba1));
+                return (Osoba1)XML.Deserialize(stream);
+            }
+        }
+    }
+}
---
+++ Serializacja/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serializacja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Osoba1 osoba1 = Osoba1.Load("Dane.xml");
+            osoba1.Imie = "Kamil";
+            osoba1.Nazwisko = "Paszkiet";
+            osoba1.Wiek = 25;
+            osoba1.Save("Dane.xml");
+
+            
+        }
+    }
+}
